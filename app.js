c9eefc8fd60d65e46a5fe45b39db5d5998e66d11
onjamm <75151498+onjamm@users.noreply.github.com>
2026-02-11 12:53:54 -0800

    Created an Admin route and styled the form a bit!

--- app.js
+++ app.js
@@ -1,17 +1,46 @@
-import express from 'express';
+import express from "express";
 
 const app = express();
 
+//new port number different from the ones we have used
 const PORT = 5000;
 
+//middlware that allows express to read form data
+//and store it in the req.body
+app.use(express.urlencoded({ extended: true }));
 
-app.use(express.static('public'));
+//create a temp array to store submitted appointments
+const appointments = [];
 
 //define a default route
-app.get('/', (req, res) => {
-    res.sendFile(`${import.meta.dirname}/views/home.html`);
+app.get("/", (req, res) => {
+  res.sendFile(`${import.meta.dirname}/views/home.html`);
 });
 
+//submit order route
+app.post("/submit-app", (req, res) => {
+  //create JSON object to store the appointment data
+  const appointment = {
+    fname: req.body.fname,
+    lname: req.body.lname,
+    date: req.body.date,
+    time: req.body.time,
+    timestamp: new Date(),
+  };
+
+  //add app to appointments array
+  appointments.push(appointment);
+
+  res.sendFile(`${import.meta.dirname}/views/confirmation.html`);
+});
+
+//admin route
+app.get("/admin", (req, res) => {
+  res.send(appointments);
+});
+
+app.use(express.static("public"));
+
 app.listen(PORT, () => {
-    console.log(`Server is running at http://localhost:${PORT}`);
+  console.log(`Server is running at http://localhost:${PORT}`);
 });
---
+++ public/js/form.js
@@ -0,0 +1,31 @@
+document.getElementById("appointment-form").onsubmit = () => {
+  clearErrors();
+
+  let isValid = true;
+
+  let fname = document.getElementById("fname").value.trim();
+  let lname = document.getElementById("lname").value.trim();
+
+  if (!fname) {
+    document.getElementById("fname-err").style.display = "block";
+    isValid = false;
+  }
+
+  if (!lname) {
+    document.getElementById("lname-err").style.display = "block";
+    isValid = false;
+  }
+
+  if (!isValid) {
+    window.scrollTo({ top: 0, left: 0, behavior: "smooth" });
+  }
+
+  return isValid;
+};
+
+function clearErrors() {
+  let errors = document.getElementsByClassName("err");
+  for (let i = 0; i < errors.length; i++) {
+    errors[i].style.display = "none";
+  }
+}
